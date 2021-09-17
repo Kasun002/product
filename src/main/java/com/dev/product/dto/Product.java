6f94521848db7c04377b40889a906e7b31ad9c54
Kasun Abaywardana <kasun.a@tailwindsw.com>
2021-09-17 22:56:04 +0530

    Tostring method added

--- src/main/java/com/dev/product/dto/Product.java
+++ src/main/java/com/dev/product/dto/Product.java
@@ -3,6 +3,7 @@ package com.dev.product.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @Data // To getters setters
 @AllArgsConstructor // To Constructor(a,b,c,d)
 @NoArgsConstructor // To Constructor ()
+@ToString // To string method
 @Entity // Specify the entety
 @Table(name = "bsr_product") // Map with DB table
 public class Product {
